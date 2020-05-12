88e88b72ada99631349beaa3645f2c6b4bffd4b9
opensea0508 <opensea0508@gmail.com>
2020-05-12 22:21:11 +0530

    whole repo edited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Avatar from "./Avatar";
+import "./App.css";
+
+const list = [
+  {
+    id: 1,
+    name: "Bhuvan",
+    subscribed: true,
+    dialog: "Bhai ke izzat dubadi",
+    link: "https://www.youtube.com/channel/UCqwUrj10mAEsqezcItqvwEw",
+  },
+  {
+    id: 2,
+    name: "Ashish",
+    subscribed: false,
+    dialog: "Bijli ka bill tera baap bharega",
+    link: "https://www.youtube.com/user/ashchanchlani",
+  },
+  {
+    id: 3,
+    name: "Mr.Carry",
+    subscribed: true,
+    dialog: "Toh kaise hai aap log",
+    link: "https://www.youtube.com/user/AddictedA1",
+  },
+  {
+    id: 4,
+    name: "Gaurav Chaudhary",
+    subscribed: false,
+    dialog: "Chaliye shuru karte hai",
+    link: "https://www.youtube.com/channel/UCOhHO2ICt0ti9KAh-QHvttQ",
+  },
+];
 
 function App() {
+  const card = list.map((card, index) => {
+    const setSubscribed = (state) => {
+      card.subscribed = state;
+      // console.log("change");
+      // console.log(card.subscribed);
+    };
+    return (
+      <Avatar
+        key={index}
+        id={card.id}
+        name={card.name}
+        subscribed={card.subscribed}
+        setSubscribed={setSubscribed}
+        dialog={card.dialog}
+        link={card.link}
+      />
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="heading">
+        <h1>Welcome to Youtubers Avatar Page</h1>
+      </div>
+
+      <div className="card-list"> {card}</div>
     </div>
   );
 }
--- src/Avatar.js
+++ src/Avatar.js
@@ -1,60 +1,31 @@
-import React,{Component} from 'react';
-import './Avatar.css';
-import Avatarlist from './Avatarlist';
-import 'tachyons';
+import React, { useState } from "react";
 
-class Avatar extends Component{
-
-	constructor(){
-		super();
-		this.state = {
-			name: 'Youtubers World'
-		}
-	}
-	namechange(){
-		const arr = ['Youtubers World','Welcome to Youtubers World :)']
-		this.setState({
-			name: arr[1]
-		})
-	}
-
-	render(){
-
-		const arraylist = [{
-		id: 1,
-		name:'Bhuvan',
-		dialog:'Bhai ke izzat dubadi',
-	},
-	{
-		id: 2,
-		name:'Ashish',
-		dialog:'Bijli ka bill tera baap bharega',
-	},
-	{
-		id: 3,
-		name:'Mr.Carry',
-		dialog:'Toh kaise hai aap log',
-	},
-	{
-		id: 4,
-		name:'Gaurav Chaudhary',
-		dialog:'Chaliye shuru karte hai',
-	},
-	]
-
-	const card = arraylist.map((avtarcard, index) =>{
-		return <Avatarlist key={index} id={arraylist[index].id}
-							name={arraylist[index].name}
-							dialog={arraylist[index].dialog}/>
-	})
-
-		return( <div className='mainpage tc'>
-			<h1> {this.state.name} </h1>
-			{card}
-			<button className='button' onClick={ () => this.namechange()}> E N T E R </button>
-			</div>
-		)
-	}
+export default function Avatar({
+  id,
+  name,
+  subscribed,
+  setSubscribed,
+  dialog,
+  link,
+}) {
+  const [subscribe, setSubscribe] = useState(subscribed);
+  const subscribeToggle = () => {
+    setSubscribe(!subscribe);
+    setSubscribed(!subscribe);
+  };
+  return (
+    <div className="card">
+      <img
+        height="220px"
+        src={`https://joeschmoe.io/api/v1/${name}`}
+        alt="Avatar"
+      />
+      <h2 className="name"> {name} </h2>
+      <h4 className="dialog">"{dialog}"</h4>
+      <button onClick={subscribeToggle} className={subscribe ? "grey" : "red"}>
+        {subscribe ? "Unsubscribe" : "Subscribe"}
+      </button>
+      <a href="{link}">Visit Channel</a>
+    </div>
+  );
 }
-
-export default Avatar;
